1dc67affd2b5c43dc6de7423058b09946eeaeee2
Luxury-Karma <alexgauv@hotmail.ca>
2023-05-24 11:44:56 -0400

    update

--- main.py
+++ main.py
@@ -6,7 +6,6 @@ import os
 import subprocess
 
 
-
 def find_all_files(start_path: str) -> list[tuple[str, list[str], list[str]]]:
     """
     :param start_path: path where we need to start to look at
@@ -19,7 +18,6 @@ def find_all_files(start_path: str) -> list[tuple[str, list[str], list[str]]]:
     return result
 
 
-
 def open_powershell() -> subprocess.Popen:
     """
     Opens a new PowerShell session in the background.
@@ -30,7 +28,6 @@ def open_powershell() -> subprocess.Popen:
         stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True
     )
     return powershell_process
-
 
 
 def send_powershell_command(process: subprocess.Popen, cmd: str):
@@ -57,16 +54,26 @@ def close_powershell(process: subprocess.Popen):
 
 # Create Directory
 def push_directory(data, ps_process, linux_dir: str):
-    #send_powershell_command(f'cd {linux_dir}')
+    #send_powershell_command(f'cd {linux_dir}')  # Go in the linux directory to add those files and dires
     for dirs, subdirs, files in data:
+        live_directory = dirs.split('\\')
+        live_directory: str = live_directory[len(live_directory)-1]
+        #send_powershell_command(f'lcd {dirs}')  # make all of the subdirectories
+        #send_powershell_command(ps_process, f'cd .\\{live_directory}')  #  place in the correct directory in the linux server
+        send_powershell_command(ps_process,f'mkdir {live_directory}')
         for subdir in subdirs:
-            send_powershell_command(ps_process, f'mkdir {subdir}')
+            send_powershell_command(ps_process, f'mkdir {subdir}')  # Create all the directories
+
+        #send_powershell_command(f'lcd {dirs}')  # Swictch the main computer to the directory of the file
         for file in files:
             file_path = os.path.join(dirs, file)
-            send_powershell_command(ps_process, f'lcd {}')
-            send_powershell_command(ps_process, f'mput {file_path}')
+            send_powershell_command(ps_process, f'mput {file}')  # send the files
 
 
+def ssh_connection(ps_process: subprocess, ipaddress: str, account: str, password: str):
+    send_powershell_command(ps_process,f'ftp {ipaddress}')
+    send_powershell_command(ps_process, f'{account}')
+    send_powershell_command(ps_process, f'{password}')
 
 
 def main(moving_directory: str):
@@ -76,12 +83,6 @@ def main(moving_directory: str):
     linux_dir = moving_directory.split('\\')
     linux_dir = linux_dir[len(linux_dir)-1]
     push_directory(data, ps_process, linux_dir)
-
-
-
-
-
-
 
 
 # Press the green button in the gutter to run the script.
